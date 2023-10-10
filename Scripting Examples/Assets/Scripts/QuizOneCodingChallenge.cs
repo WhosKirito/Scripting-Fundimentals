ce1dc6fed24173b340e5670ee0a8c73102c8ae61
WhosKirito <dominickhawkins738@gmail.com>
2023-10-10 13:59:26 -0700

    Update code challenge

---
+++ Scripting Examples/Assets/Scripts/QuizOneCodingChallenge.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOneCodingChallenge : MonoBehaviour
+{
+    public int Score = 0;
+    public string PlayerName = "Dominick";
+    public int Health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncreaseScore();
+        PrintPlayerName();
+        DecreaseHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void IncreaseScore()
+    {
+        Score += 10;
+        
+    }
+
+    private void PrintPlayerName()
+    {
+        Debug.Log(PlayerName);
+    }
+
+    private void DecreaseHealth()
+    {                                                           
+        Health -= 20;
+    }
+}
